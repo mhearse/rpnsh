a9ee86dbf38a556a5d838cfdae0b38b93a0994ec
mhearse <matt_hersant@yahooo.com>
2017-10-25 20:55:42 -0700

    adding trash examples

---
+++ trash/me.py
@@ -0,0 +1,11 @@
+import readline
+
+def completer(text, state):
+    options = [i for i in commands if i.startswith(text)]
+    if state < len(options):
+        return options[state]
+    else:
+        return None
+
+readline.parse_and_bind("tab: complete")
+readline.set_completer(completer)
